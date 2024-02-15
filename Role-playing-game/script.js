d4c30e2521ffd28008e4ef026145b9dfaae747f7
unknown <craigkapan@gmail.com>
2024-02-15 17:36:01 +0200

    By step 151

--- Role-playing-game/script.js
+++ Role-playing-game/script.js
@@ -175,8 +175,12 @@ function fightDragon() {
 function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
-  health -= monsters[fighting].level;
+  health -= getMonsterAttackValue(monsters[fighting].level);
+  if (isMonsterHit()) {
   monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+  } else {
+    text.innerText += " You miss.";
+  }
   healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
   if (health <= 0) {
@@ -188,6 +192,15 @@ function attack() {
     defeatMonster();
     }
   }
+}
+
+function getMonsterAttackValue(level) {
+  const hit = (level * 5) - (Math.floor(Math.random() * xp));
+  return hit > 0 ? hit : 0;
+}
+
+function isMonsterHit() {
+  return Math.random() > .2 || health < 20;
 }
 
 function dodge() {
