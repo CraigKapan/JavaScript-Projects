8c6a32a895ad154dd8fd0e94a68f18af1837df29
unknown <craigkapan@gmail.com>
2024-04-02 19:54:26 +0200

    Adding questions

--- Quiz-Game/script.js
+++ Quiz-Game/script.js
@@ -7,6 +7,109 @@ const OptionB = document.querySelector(".B");
 const OptionC = document.querySelector(".C");
 const OptionD = document.querySelector(".D");
 
+const QuestionObj = [
+  {
+    "Question Number" : 1,
+    "Questiion" : "How many squares are on a chess board",
+    "A" : 16,
+    "B" : 25,
+    "C" : 50,
+    "D" : 64,
+
+    "Answer" : "D"
+  },
+  {
+    "Question Number" : 2,
+    "Questiion" : "How many Continents are in the world",
+    "A" : 7,
+    "B" : 3,
+    "C" : 5,
+    "D" : 10,
+
+    "Answer" : "A"
+  },
+  {
+    "Question Number" : 3,
+    "Questiion" : "What's the answer to this : 2x - 8 = 4x + 2",
+    "A" : 0,
+    "B" : -5,
+    "C" : 10,
+    "D" : 400,
+
+    "Answer" : "B"
+  },
+  {
+    "Question Number" : 4,
+    "Questiion" : "How many squares are on a chess board",
+    "A" : 16,
+    "B" : 25,
+    "C" : 50,
+    "D" : 64,
+
+    "Answer" : "D"
+  },
+  {
+    "Question Number" : 5,
+    "Questiion" : "What does I.T stand for",
+    "A" : "Information Technology",
+    "B" : "Inquizitive Technology",
+    "C" : "Information Tactics",
+    "D" : "Impatient Tacion",
+
+    "Answer" : "A"
+  },
+  {
+    "Question Number" : 6,
+    "Questiion" : "How many squares are on a chess board",
+    "A" : 16,
+    "B" : 25,
+    "C" : 50,
+    "D" : 64,
+
+    "Answer" : "D"
+  },
+  {
+    "Question Number" : 7,
+    "Questiion" : "How many squares are on a chess board",
+    "A" : 16,
+    "B" : 25,
+    "C" : 50,
+    "D" : 64,
+
+    "Answer" : "D"
+  },
+  {
+    "Question Number" : 8,
+    "Questiion" : "How many squares are on a chess board",
+    "A" : 16,
+    "B" : 25,
+    "C" : 50,
+    "D" : 64,
+
+    "Answer" : "D"
+  },
+  {
+    "Question Number" : 9,
+    "Questiion" : "How many squares are on a chess board",
+    "A" : 16,
+    "B" : 25,
+    "C" : 50,
+    "D" : 64,
+
+    "Answer" : "D"
+  },
+  {
+    "Question Number" : 10,
+    "Questiion" : "How many squares are on a chess board",
+    "A" : 16,
+    "B" : 25,
+    "C" : 50,
+    "D" : 64,
+
+    "Answer" : "D"
+  },
+]
+
 const Init = function () {
   Intropage.classList.remove("hidden");
   Questionbox.classList.add("hidden");
