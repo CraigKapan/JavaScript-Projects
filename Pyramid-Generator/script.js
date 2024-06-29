4e0f38bf11612f04e894296e93b8843dfa8f0abf
unknown <craigkapan@gmail.com>
2024-06-29 21:11:47 +0200

    Step 47

--- Pyramid-Generator/script.js
+++ Pyramid-Generator/script.js
@@ -3,9 +3,14 @@ const count = 8;
 const rows = [];
 
 for (let i = 0; i < count; i++) {
-    rows.push(i); 
+    rows.push(character); 
+    character.repeat(i + 1);
 }
 
 let result = "";
+
+for (const row of rows) {
+    result += "\n" + row;
+}
 
 console.log(result);
