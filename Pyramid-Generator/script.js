d12cb0f8298082711b191e153ad865f7bb83c354
unknown <craigkapan@gmail.com>
2024-06-29 22:11:00 +0200

    Step 51

--- Pyramid-Generator/script.js
+++ Pyramid-Generator/script.js
@@ -2,9 +2,14 @@ const character = "#";
 const count = 8;
 const rows = [];
 
+function padRow() {
+    return "Hello!"
+}
+const call = padRow();
+console.log(call);
+
 for (let i = 0; i < count; i++) {
-    rows.push(character); 
-    character.repeat(i + 1);
+    rows.push(character.repeat(i + 1));
 }
 
 let result = "";
