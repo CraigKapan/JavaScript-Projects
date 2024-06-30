7351de34f5f6338aa20bfb8c402d3af1a8d03114
unknown <craigkapan@gmail.com>
2024-06-30 19:35:36 +0200

    Step 70

--- Pyramid-Generator/script.js
+++ Pyramid-Generator/script.js
@@ -2,14 +2,12 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-function padRow() {
-    return "Hello!"
+function padRow(rowNumber, rowCount) {
+    return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
-const call = padRow();
-console.log(call);
 
 for (let i = 0; i < count; i++) {
-    rows.push(character.repeat(i + 1));
+    rows.push(padRow(i + 1, count))
 }
 
 let result = "";
