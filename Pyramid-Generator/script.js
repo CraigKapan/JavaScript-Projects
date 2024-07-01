003212d73be622616c88c98312473f177c8f4a8a
unknown <craigkapan@gmail.com>
2024-07-01 20:05:28 +0200

    Step 91

--- Pyramid-Generator/script.js
+++ Pyramid-Generator/script.js
@@ -1,19 +1,37 @@
+"use strict";
+
 const character = "#";
 const count = 8;
 const rows = [];
 
 function padRow(rowNumber, rowCount) {
-    return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+  return (
+    " ".repeat(rowCount - rowNumber) +
+    character.repeat(2 * rowNumber - 1) +
+    " ".repeat(rowCount - rowNumber)
+  );
 }
 
-for (let i = 0; i < count; i++) {
-    rows.push(padRow(i + 1, count))
+// Change code to different type of loop
+/* for (let i = 1; i <= count; i++) {
+    rows.push(padRow(i, count))
+} */
+
+let continueLoop = false;
+let done = 0;
+
+while (done !== count) {
+    done++;
+    rows.push(padRow(done, count));
+    if (done === count) {
+        continueLoop = false;
+    }
 }
 
 let result = "";
 
 for (const row of rows) {
-    result += "\n" + row;
+  result += "\n" + row;
 }
 
 console.log(result);
